900f4dece419364739259173a9c66c6740073d9e
Jianlong Chen <jianlong99@gmail.com>
2019-11-12 19:44:30 +0800

    Use go-arg to parse argument

--- binlog/parser.go
+++ binlog/parser.go
@@ -8,7 +8,10 @@ package main
 import (
 	"bytes"
 	"encoding/binary"
+	"errors"
 	"fmt"
+	"github.com/alexflint/go-arg"
+	"io"
 	"os"
 )
 
@@ -217,36 +220,103 @@ func NewBinLogEvent(header *BinLogEventHeader, text []byte) BinLogEvent {
 	}
 }
 
-func usage() {
-	fmt.Printf("Usage: %s <binlog>\n", os.Args[0])
-	os.Exit(2)
+type Parser struct {
+	file *os.File
+	text []byte
 }
 
-func readEventBody(file *os.File, header *BinLogEventHeader, text *[]byte) error {
-	size := header.EventSize - BINLOG_EVENT_HEADER_LEN
-	if size == 0 {
-		return nil
+func (self *Parser) readEventHeader() (*BinLogEventHeader, error) {
+	self.text = self.text[0:BINLOG_EVENT_HEADER_LEN]
+	n, err := self.file.Read(self.text)
+	if err != nil {
+		return nil, err
 	}
 
-	*text = (*text)[0:size]
-	n, err := file.Read(*text)
+	if n != BINLOG_EVENT_HEADER_LEN {
+		return nil, errors.New("Failed to read event header")
+	}
+
+	return NewBinLogEventHeader(self.text)
+}
+
+func (self *Parser) ReadEvent() (BinLogEvent, error) {
+	header, err := self.readEventHeader()
+	if err != nil {
+		return nil, err
+	}
+
+	size := header.EventSize - BINLOG_EVENT_HEADER_LEN
+	if size != 0 {
+		if uint32(cap(self.text)) < size {
+			self.text = make([]byte, size)
+		} else {
+			self.text = self.text[:size]
+		}
+
+		n, err := self.file.Read(self.text)
+		if err != nil {
+			return nil, err
+		}
+
+		if n != len(self.text) {
+			return nil, errors.New("Failed to read event body")
+		}
+	}
+
+	return NewBinLogEvent(header, self.text), nil
+}
+
+func (self *Parser) SkipEvent() error {
+	header, err := self.readEventHeader()
 	if err != nil {
 		return err
 	}
 
-	if n != len(*text) {
-		panic("Failed to read event body")
+	size := header.EventSize - BINLOG_EVENT_HEADER_LEN
+	if size != 0 {
+		if _, err = self.file.Seek(int64(size), 1); err != nil {
+			return err
+		}
 	}
 
 	return nil
 }
 
-func main() {
-	if len(os.Args) < 2 {
-		usage()
+func NewParser(file *os.File) (*Parser, error) {
+	text := make([]byte, 4, 1024)
+	n, err := file.Read(text)
+	if err != nil {
+		return nil, err
 	}
 
-	file, err := os.Open(os.Args[1])
+	if n != 4 {
+		return nil, errors.New("Failed to read binlog file header")
+	}
+
+	if text[0] != 0xfe || text[1] != 'b' || text[2] != 'i' || text[3] != 'n' {
+		return nil, errors.New("Invalid binlog file header")
+	}
+
+	parser := new(Parser)
+	parser.file = file
+	parser.text = text
+	return parser, nil
+}
+
+func usage() {
+	fmt.Printf("Usage: %s <binlog>\n", os.Args[0])
+	os.Exit(2)
+}
+
+func main() {
+	var args struct {
+		Path  string `arg:"-p,required" help:"binlog path"`
+		Start int    `arg:"-s" default:"0" help:"start event"`
+		Count int    `arg:"-c" default:"-1" help:"show event count"`
+	}
+
+	arg.MustParse(&args)
+	file, err := os.Open(args.Path)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(-1)
@@ -254,38 +324,31 @@ func main() {
 
 	defer file.Close()
 
-	text := make([]byte, 4, 1024*1024)
-	var n int
-	n, err = file.Read(text)
+	var parser *Parser
+	parser, err = NewParser(file)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(-1)
 	}
 
-	if n != 4 {
-		panic("Failed to read binlog file header")
+	for i := 0; i < args.Start; i++ {
+		if err = parser.SkipEvent(); err != nil {
+			panic(err)
+		}
 	}
 
-	if text[0] != 0xfe || text[1] != 'b' || text[2] != 'i' || text[3] != 'n' {
-		panic("Invalid binlog file header")
-	}
+	for i := 0; args.Count < 0 || i < args.Count; i++ {
+		event, err := parser.ReadEvent()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
 
-	text = text[0:BINLOG_EVENT_HEADER_LEN]
-	n, err = file.Read(text)
-	if n != len(text) {
-		panic("Failed to read binlog event header")
-	}
+			panic(err)
+		}
 
-	header, err := NewBinLogEventHeader(text)
-	if err != nil {
-		panic(err)
+		fmt.Printf("----------------------EVENT-------------------\n")
+		fmt.Printf("header: %s\npostHeader: %s\npayload: %s\n",
+			event.GetHeader(), event.GetPostHeader(), event.GetPayload())
 	}
-
-	if err = readEventBody(file, header, &text); err != nil {
-		panic(err)
-	}
-
-	event := NewBinLogEvent(header, text)
-	fmt.Printf("header: %s\npostHeader: %s\npayload: %s\n",
-		event.GetHeader(), event.GetPostHeader(), event.GetPayload())
 }
